ebba09cfb7ff2bdf092310f2b87cc5bb13e0dd10
Santiago Prado <74788002+santiagoprado12@users.noreply.github.com>
2024-10-29 22:49:00 -0500

    Update test_api.py

--- tests/api/test_api.py
+++ tests/api/test_api.py
@@ -5,20 +5,49 @@ from src.api.main import app
 client = TestClient(app)
 
 
-def test_null_prediction():
-    response = client.post("/v1/prediction",
-                           json={
-                               "PassengerId": 0,
-                               "Pclass": 0,
-                               "Name": "string",
-                               "Sex": "male",
-                               "Age": 0,
-                               "SibSp": 0,
-                               "Parch": 0,
-                               "Ticket": "string",
-                               "Fare": 0,
-                               "Cabin": "string",
-                               "Embarked": "S"
-                           })
+def test_prediction():
+    response = client.post(
+        "/v1/prediction",
+        json={
+            "PassengerId": 0,
+            "Pclass": 0,
+            "Name": "string",
+            "Sex": "male",
+            "Age": 0,
+            "SibSp": 0,
+            "Parch": 0,
+            "Ticket": "string",
+            "Fare": 0,
+            "Cabin": "string",
+            "Embarked": "S",
+        },
+    )
     assert response.status_code == 200
     assert response.json()["Survived"] in (0, 1)
+
+
+def test_batch_prediction():
+    response = client.post(
+        "/v1/batch_prediction",
+        json={
+            "batch_data": [
+                {
+                    "PassengerId": 0,
+                    "Pclass": 0,
+                    "Name": "string",
+                    "Sex": "male",
+                    "Age": 0,
+                    "SibSp": 0,
+                    "Parch": 0,
+                    "Ticket": "string",
+                    "Fare": 0,
+                    "Cabin": "string",
+                    "Embarked": "S",
+                }
+            ]
+        },
+    )
+    assert response.status_code == 200
+    assert isinstance(response.json()["Survived"], list) 
+    for prediction in response.json()["Survived"]:
+        assert prediction in (0, 1)
